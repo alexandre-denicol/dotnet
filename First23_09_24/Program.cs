ebca3d4ad4eb8754fbdeb270b2278592d6891196
alexandre.denicol <alexandre.denicol@gmail.com>
2024-09-23 21:51:20 -0300

    CHORE:23_09_24

---
+++ First23_09_24/Program.cs
@@ -0,0 +1,13 @@
+﻿//First C# code - 23/09/2024
+string course = "Welcome to my first C# code";
+Console.WriteLine(course);
+string name = "Alexandre";
+string secName = "Denicol";
+Console.WriteLine(name);
+Console.WriteLine(secName);
+
+
+
+
+
+
