6903cf04156a346c9de5d93c97fac151744869d7
alexandre.denicol <alexandre.denicol@gmail.com>
2024-09-24 21:23:36 -0300

    Prática 24/09/24

---
+++ Lista24_09_24/Lista.cs
@@ -0,0 +1,36 @@
+﻿// Exercício prático 24_09 - Menu e Lista
+
+using System.Collections.Generic;
+int averageScore = 5;
+
+if(averageScore >= 5){
+    Console.WriteLine("Nota suficiente para aprovação");
+}
+else{
+    Console.WriteLine("Nota insuficiente para aprovação");
+}
+
+List<string> languanges = new List<string>();
+
+languanges.Add("C#");
+languanges.Add("Java");
+languanges.Add("JS");
+
+Console.WriteLine(languanges[0]);
+
+void SearchPosition(int a){
+    if (a > 2){
+        Console.WriteLine("Não há nada nessa posição");
+    }
+    else{
+        Console.WriteLine($"A linguagem nessa posicção é: {languanges[a]}");
+    }
+}
+
+void ReadPosition(){
+    Console.Write("Qual a posição vc quer saber? ");
+    int num = int.Parse(Console.ReadLine());
+    SearchPosition(num);
+}
+
+ReadPosition();
